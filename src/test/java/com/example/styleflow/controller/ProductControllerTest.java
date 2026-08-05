99ea0706328182685d718b2844ba5e907946846b
Zakaria-Ar <araouizakaria@gmail.com>
2026-08-05 11:25:12 +0100

    add integration test

---
+++ src/test/java/com/example/styleflow/controller/ProductControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.styleflow.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; //the tool to send fake HTTP requests
+
+    @Test
+    void shouldReturn404ForMissingProduct() throws Exception {
+        mockMvc.perform(get("/api/products/999"))     // send GET
+                .andExpect(status().isNotFound());     // assert 404
+    }
+
+    @Test
+    void shouldReturn201ForCreatingProduct() throws Exception {
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"Shirt\",\"price\":200,\"stockQuantity\":5,\"category\":\"Tops\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Shirt"))    // assert the JSON body
+                .andExpect(jsonPath("$.id").exists());           // id was generated
+    }
+
+    @Test
+    void shouldReturn400ForBadRequest() throws Exception {
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"price\":200}"))
+                .andExpect(status().isBadRequest());
+    }
+}
