6442b121d03876ba101afed1ebe31b0702f2f477
Zakaria-Ar <araouizakaria@gmail.com>
2026-08-29 00:24:26 +0100

    all tests fixed

--- src/test/java/com/example/styleflow/StyleFlowApplicationTests.java
+++ src/test/java/com/example/styleflow/StyleFlowApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StyleFlowApplicationTests {
+class StyleFlowApplicationTests extends IntegrationTestBase {
 
     @Test
     void contextLoads() {
