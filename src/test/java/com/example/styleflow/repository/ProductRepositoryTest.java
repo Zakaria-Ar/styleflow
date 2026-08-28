4d3233676aba15986c9a2a78a30394d2f8509851
Zakaria-Ar <araouizakaria@gmail.com>
2026-08-28 23:46:26 +0100

    fix CI pipeline to run version problem

--- src/test/java/com/example/styleflow/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/styleflow/repository/ProductRepositoryTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,7 +17,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @Testcontainers
-@TestPropertySource(properties = "spring.flyway.enabled=false")
 class ProductRepositoryTest {
 
     @Container
